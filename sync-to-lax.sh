4246b8a0f186f43ee52687148c5f053a19c4dd97
Linus Kendall <git@linuskendall.com>
2021-07-02 12:46:52 +0530

    Moved sync location to /solana

--- sync-to-lax.sh
+++ sync-to-lax.sh
@@ -11,6 +11,7 @@ exec 1>>/home/solana/rsync.log 2>&1
 echo -n "Starting sync on "
 date
 
-rsync -avh -e ssh --progress /solana/ledger/tower-DDnAqxJVFo2GVTujibHt5cjevHMSE9bo8HJaydHoshdp.bin solana-lax:/solana/ledger
+rsync -avh -e ssh --progress /solana/ledger/tower-DDnAqxJVFo2GVTujibHt5cjevHMSE9bo8HJaydHoshdp.bin solana-lax:/solana/
+
 
 echo
